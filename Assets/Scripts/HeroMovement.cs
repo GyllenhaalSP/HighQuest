bfabf586ad94bf209c8bb4570f39d253e7ff695f
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-12-08 03:18:54 +0100

    Assets para interiores e intento de mapa

--- Assets/Scripts/HeroMovement.cs
+++ Assets/Scripts/HeroMovement.cs
@@ -7,10 +7,12 @@ public class HeroMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidBody;
     private Vector3 change;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -18,13 +20,27 @@ public class HeroMovement : MonoBehaviour
     void Update()
     {
         change = Vector3.zero;
-        change.x = Input.GetAxisRaw("Horizontal");
-        change.y = Input.GetAxisRaw("Vertical");
-        if(change != Vector3.zero)
-        {
-            MoveCharacter();
-        }
+        change.x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        change.y = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+        UpdateAnimationAndMove();
     }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            transform.Translate(new Vector3(change.x, change.y));
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+        MoveCharacter();
+    }
+
 
     void MoveCharacter()
     {
